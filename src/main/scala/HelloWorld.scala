4b7bbe0ab2e9dfbe1a653967c1e0ead3703f5cb1
oljh <https://github.com/oljh/MyCheese.git>
2019-09-25 17:06:50 +0300

    Scala Foo

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,15 @@
 object HelloWorld {
   def main(args:Array[String]) : Unit = {
     println("Hello World")
+    class Foo(x: String){
+      def bar(y: String) = x + y;
+    }
+
+    val foo = new Foo("sda")
+
+    foo.bar("sada")
+    println(foo.bar("asda"))
   }
 }
+
+
